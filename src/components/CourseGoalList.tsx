acc987e3638d79f5af2a660b47a20b77cbd0662a
Antoine Birkhofer <birkhoferantoine@gmail.com>
2024-02-19 16:46:48 +0100

    Infobox and severity done, project complete

--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -1,6 +1,7 @@
 import {type FC, ReactNode} from "react";
 import {type CourseGoal as CGType} from "../App.tsx";
 import CourseGoal from "./CourseGoal";
+import Infobox from "./Infobox.tsx";
 
 type CourseGoalListProps = {
     goals: CGType[];
@@ -8,19 +9,33 @@ type CourseGoalListProps = {
 };
 
 const CourseGoalList: FC<CourseGoalListProps> = ({goals, onDeleteGoal}) => {
+    let warningBox: ReactNode;
+
+    if (goals.length >= 4) {
+        warningBox = (
+            <Infobox mode={"warning"} severity={'medium'}>
+                You're collecting a lot of Goals, don't overdo it !
+            </Infobox>
+        )
+    }
+
     return (
-        <ul>
-            {goals.map(({id, title, description}): ReactNode => {
-                return (
-                    <li key={id}>
-                        <CourseGoal id={id} title={title} onDelete={onDeleteGoal}>
-                            <p>{description}</p>
-                        </CourseGoal>
-                    </li>
-                );
-            })}
-        </ul>
+        <>
+            {warningBox}
+            <ul>
+                {goals.map(({id, title, description}): ReactNode => {
+                    return (
+                        <li key={id}>
+                            <CourseGoal id={id} title={title} onDelete={onDeleteGoal}>
+                                <p>{description}</p>
+                            </CourseGoal>
+                        </li>
+                    );
+                })}
+            </ul>
+        </>
     );
+
 };
 
 export default CourseGoalList;
